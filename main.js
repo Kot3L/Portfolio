ece3cb2da5f6ddd8b94b7061f8dfca99ef874289
walnietykotel <s.rzeszo@hotmail.com>
2024-04-07 14:24:25 +0200

    Added stuff

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ window.onscroll = function () {
   addBorder();
 };
 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 110);
+};
+
 function addBorder() {
   if (document.documentElement.scrollTop > 1 || window.pageYOffset > 1) {
     header.classList.add("lowBorder");
@@ -11,3 +15,11 @@ function addBorder() {
     header.classList.remove("lowBorder");
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  window.onload = scrollToTop;
+});
